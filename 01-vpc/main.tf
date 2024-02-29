6fb8bdd736764083bfd17ab54b16003c7d23c13a
sowji <jujjurisowjanya@gmail.com>
2024-02-29 22:00:16 +0530

    new

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -4,6 +4,7 @@
     environment = var.environment
     common_tags = var.common_tags
     vpc_tags = var.vpc_tags
+    vpc_cidr = var.vpc_cidr
     # public subnet
     public_subnets_cidr = var.public_subnets_cidr
     private_subnets_cidr = var.private_subnets_cidr
