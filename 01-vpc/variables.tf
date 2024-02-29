7c1fff665055f2a1ad5b427c56a14d08a0b33858
sowji <jujjurisowjanya@gmail.com>
2024-02-29 20:39:51 +0530

    new

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -19,13 +19,13 @@ variable "environment" {
     default = "prod"
 }
 variable "public_subnets_cidr" {
-    default = ["10.1.1.0/24" , "10.1.2.0/24"]
+    default = ["10.1.1.0/24", "10.1.2.0/24"]
 }
 variable "private_subnets_cidr" {
-    default = ["10.1.11.0/24" , "10.1.12.0/24"]
+    default = ["10.1.11.0/24", "10.1.12.0/24"]
 }
 variable "database_subnets_cidr" {
-    default = ["10.1.21.0/24" , "10.1.22.0/24"]
+    default = ["10.1.21.0/24", "10.1.22.0/24"]
 }
 variable "is_peering_required" {
     default = true
